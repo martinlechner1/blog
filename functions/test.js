c6d093d10cba4220e1b56b794963a35b9d1da6dc
martinlechner1 <martin.lechner.ml@gmail.com>
2019-10-25 22:23:28 +0200

    Return event + context

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: "No worries, all is working fine!",
+    body: event + context + "No worries, all is working fine!",
   })
 }
