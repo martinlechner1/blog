49ac864448ce7586869931022baec64f59d56d84
martinlechner1 <martin.lechner.ml@gmail.com>
2019-10-25 22:29:09 +0200

    Stringify helps...

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,9 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: event + context + "No worries, all is working fine!",
+    body:
+      JSON.stringify(event) +
+      JSON.stringify(context) +
+      "No worries, all is working fine!",
   })
 }
