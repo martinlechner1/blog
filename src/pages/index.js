03f1a1e15d80b1adbc25a7b287b7909499c1ab3f
Martin Lechner <martin.lechner.ml@gmail.com>
2023-10-01 12:09:48 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,42 +17,9 @@ const BlogIndex = props => {
         keywords={[`portfolio`, `scala`, `blog`, `react`]}
       />
       <h2>Hi, my name is Martin,</h2>
-      <p>
-        functional programming advocate and full stack engineer at{" "}
-        <strong>Autoscout24</strong>. I love crafting clean code, building great
-        teams, coaching other folks and building data driven products. My tech
-        toolbox includes Scala, Typescript, Node, React and AWS.
-      </p>
-      <p>
-        I've been addicted to programming since school, launching my first
-        bigger webapp while studying at <strong>Helmholtz-Zentrum</strong> to
-        visualize biological networks. After a year at{" "}
-        <strong>Telefonica Germany</strong> moving data around and analyzing the
-        data warehouse, I started my first full time job at{" "}
-        <strong>Jambit</strong>, a munich based software consultancy. Working on
-        many different projects with good people helped my learn quite a lot in
-        the time being there.
-      </p>
-      <p>
-        When looking for a new challenge, I stumbled upon{" "}
-        <strong>Autoscout24</strong>, which impressed me with their tech stack
-        and full dev-ops driven approach. Being new to this mindset led to a
-        steep learning curve, picking up{" "}
-        <a
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: "var(--textLink)",
-          }}
-          href="https://medium.com/scout24-engineering/what-i-learned-in-my-first-year-at-autoscout24-57266cd3246b"
-        >
-          many things in my first year.
-        </a>{" "}
-        Currently I'm driving functional programming adoption with the typelevel
-        ecosystem, building ai driven data products and coaching other
-        developers.
-      </p>
-    </Layout>
+     <p>I work on developer experience at Personio and care a lot about people, tech and good coffee.
+          </p>
+          </Layout>
   )
 }
 
